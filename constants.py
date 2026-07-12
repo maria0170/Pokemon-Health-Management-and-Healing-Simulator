1fdb9e8a0017acc4f44fe2f6f59a02b3da72dc84
Mariya Mushasta <mushastam@gmail.com>
2026-07-12 18:46:57 -0400

    Add healing and cost constants for potions
    Define constants for potion healing and costs.

---
+++ constants.py
@@ -0,0 +1,7 @@
+POTION_HEAL = 20               # amount of healing done by a potion
+SUPER_POTION_HEAL = 50         # amount of healing done by a super potion
+POTION_COST = 0.99             # cost of a potion
+SUPER_POTION_COST = 2.49       # cost of a super potion
+ANTIDOTE_BASE_COST = 2.99      # base cost of an antidote before savings
+REVIVE_COST_PER_HEALTH = 0.02  # revive cost multiplier (per health)
+REVIVE_COST_CAP = 9.99         # maximum revive cost (capped value)
